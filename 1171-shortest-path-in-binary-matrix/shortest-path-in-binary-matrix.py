c44fba01682c9252126da562151c34cb5817aba7
Kirpal Singh <122300834+kirpalsingh225@users.noreply.github.com>
2026-03-14 15:11:38 +0530

    Time: 113 ms (96.20%) | Memory: 19.8 MB (88.81%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,23 +1,46 @@
+from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
+        
+        m, n = len(grid), len(grid[0])
+
+        if m==0 or n==0 or grid[0][0]!=0:
             return -1
 
-        queue = collections.deque([(0, 0, 1)]) # (row, col, distance)
-        grid[0][0] = 1 # Mark as visited
 
-        while queue:
-            r, c, d = queue.popleft()
-            if r == n - 1 and c == n - 1:
-                return d
-            
-            # Explore all **8 neighbors**
-            for dr in range(-1, 2):
-                for dc in range(-1, 2):
-                    if dr == 0 and dc == 0: continue
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0:
-                        grid[nr][nc] = 1
-                        queue.append((nr, nc, d + 1))
+        q = deque()
+
+        q.append([0, 0])
+        grid[0][0] = 1
+
+        level = 0
+
+        directions = [(1, 1), (0, 1), (1, 0), (-1,0), (0, -1), (-1,-1), (1, -1), (-1, 1)] 
+        while q:
+            l = len(q)
+
+            for i in range(l):
+                x, y = q.popleft()
+
+                if x == m-1 and y == n-1:
+                    return level+1
+
+                for dirs in directions:
+                    new_x = x+dirs[0]
+                    new_y = y+dirs[1]
+
+                    if new_x<0 or new_x>=m or new_y< 0 or new_y>=n or grid[new_x][new_y]==1:
+                        continue
+
+
+                    q.append([new_x, new_y])
+                    grid[new_x][new_y] = 1
+
+            level+=1
+
+
         return -1
+
+ 
+
+
